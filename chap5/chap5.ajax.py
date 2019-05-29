1a208a5e4e047b15b3212c63a5fb9132608e17b1
februarysea <857254373@qq.com>
2019-05-29 17:13:35 +0800

    爬到微博妹子了 开心

--- chap5/chap5.ajax.py
+++ chap5/chap5.ajax.py
@@ -1,7 +1,7 @@
 from urllib.parse import urlencode
 from pyquery import PyQuery as pq
 import requests
-base_url = "https://m.weibo.cn/api/container/getIndex?"
+baseUrl = "https://m.weibo.cn/comments/hotflow?"
 
 headers = {
     'Host': "m.weibo.cn",
@@ -10,5 +10,45 @@ headers = {
     'X-Requested-With': "XMLHttpRequest",
 }
 
-r = requests.get(url="https://m.weibo.cn/detail/4365090332076737", headers=headers)
-print(r)
+
+def get_page(max_id):
+    params = {
+        'id': '4365090332076737',
+        'mid': '4365090332076737',
+        'max_id': str(max_id),
+        'max_id_type': '0'
+    }
+    url = baseUrl + urlencode(params)
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            json = response.json()
+    except requests.ConnectionError as e:
+        print('Error', e.args)
+
+    max_number = json.get('data').get('max_id')
+    datas = json.get('data').get('data')
+    for data in datas:
+        big = []
+        comment = {}
+        user = data.get('user')
+        pic = data.get('pic')
+        comment['usr'] = user.get('screen_name')
+        try:
+            comment['pic'] = pic.get('url')
+        except:
+            continue
+        print(comment)
+        r = requests.get(comment['pic'], stream=True)
+        with open(comment['usr']+"'.jpg'", 'wb') as fd:
+            for chunk in r.iter_content():
+                fd.write(chunk)
+
+    return max_number
+
+
+max = get_page(0)
+for i in range(0, 7):
+    max = get_page(max)
+
+
