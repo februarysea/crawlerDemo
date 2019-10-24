249010bc61b92152935c547eead80a58ec814ffb
februarysea <31513558+februarysea@users.noreply.github.com>
2019-10-24 09:45:28 +0800

    Delete chap4.xpath.py

--- chap4/chap4.xpath.py
+++
@@ -1,8 +0,0 @@
-from lxml import etree
-
-text = '''
-<li class="li li-first" name='item'><a href="link.html">first item</a></li>
-'''
-html = etree.parse('./test.html', etree.HTMLParser())
-result = html.xpath('//li[1]/child::[@href="link1.html"]')
-print(result)
